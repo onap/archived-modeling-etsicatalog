dddb97b7f1d5ac85c4700a674e9cadcdb7ac2b63
hongyuzhao <zhao.hongyu@zte.com.cn>
2020-02-04 17:46:11 +0800

    Remove redis config
    Change-Id: I419fb5d39a46a9ca2b9a74f2a4d6d4ff12101d03
    Issue-ID: MODELING-307
    Signed-off-by: hongyuzhao <zhao.hongyu@zte.com.cn>

--- docker/instance_config.sh
+++ docker/instance_config.sh
@@ -20,15 +20,15 @@ MYSQL_IP=`echo $MYSQL_ADDR | cut -d: -f 1`
 MYSQL_PORT=`echo $MYSQL_ADDR | cut -d: -f 2`
 echo "MYSQL_ADDR=$MYSQL_ADDR"
 
-if [ $REDIS_ADDR ]; then
-    REDIS_IP=`echo $REDIS_ADDR | cut -d: -f 1`
-else
-    REDIS_IP="$MYSQL_ADDR"
-fi
+# if [ $REDIS_ADDR ]; then
+#     REDIS_IP=`echo $REDIS_ADDR | cut -d: -f 1`
+# else
+#     REDIS_IP="$MYSQL_ADDR"
+# fi
 
 
 sed -i "s|DB_IP.*|DB_IP = '$MYSQL_IP'|" modeling/etsicatalog/catalog/pub/config/config.py
 sed -i "s|DB_PORT.*|DB_PORT = $MYSQL_PORT|" modeling/etsicatalog/catalog/pub/config/config.py
-sed -i "s|REDIS_HOST.*|REDIS_HOST = '$REDIS_IP'|"modeling/etsicatalog/catalog/pub/config/config.py
+# sed -i "s|REDIS_HOST.*|REDIS_HOST = '$REDIS_IP'|"modeling/etsicatalog/catalog/pub/config/config.py
 
 cat modeling/etsicatalog/catalog/pub/config/config.py
