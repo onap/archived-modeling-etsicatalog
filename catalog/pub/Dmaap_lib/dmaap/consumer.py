7dc6d1d161721f836f3d7a00de4324955d4629fd
dyh <dengyuanhong@chinamobile.com>
2020-05-27 15:56:13 +0800

    update Dmaap lib
    Issue-ID: MODELING-366
    Change-Id: I69f86ef9a90423e33e3903dcc1be63028e1571b6
    Signed-off-by: dyh <dengyuanhong@chinamobile.com>

--- catalog/pub/Dmaap_lib/dmaap/consumer.py
+++ catalog/pub/Dmaap_lib/dmaap/consumer.py
@@ -1,48 +1,47 @@
-# Copyright (c) 2019, CMCC Technologies Co., Ltd.
-# Licensed under the Apache License, Version 2.0 (the "License")
+# Copyright (c) 2019, CMCC Technologies. Co., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# http://www.apache.org/licenses/LICENSE-2.0
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 import base64
+import datetime
 import hmac
 import json
 import logging
-import datetime
-import requests
 from hashlib import sha1
 
-from ..pub.exceptions import DmaapClientException
+import requests
+
+from catalog.pub.Dmaap_lib.pub.exceptions import DmaapClientException
 
 logger = logging.getLogger(__name__)
 
 
 class ConsumerClient:
-    def __init__(self, host, topic, consumer_group, consumer_id, timeout_ms=-1, limit=-1, filter='',
-                 api_key='', api_secret=''):
-        self.host = host
+    def __init__(self, base_url, topic, consumer_group, consumer_id, timeout_ms=-1, limit=-1, filter=''):
+        self.base_url = base_url
         self.topic = topic
         self.group = consumer_group
         self.comsumer_id = consumer_id
         self.timeout_ms = timeout_ms
         self.limit = limit
         self.filter = filter
-        self.api_key = api_key
-        self.api_secret = api_secret
 
     def set_api_credentials(self, api_key, api_secret):
-
         self.api_key = api_key
         self.api_secret = api_secret
 
     def create_url(self):
-        url = "http://%s/events/%s/%s/%s" % (self.host, self.topic, self.group, self.comsumer_id)
+        url = self.base_url + "/events/%s/%s/%s" % (self.topic, self.group, self.comsumer_id)
         add_url = ""
         if self.timeout_ms > -1:
             add_url += "timeout=%s" % self.timeout_ms
@@ -81,7 +80,8 @@ class ConsumerClient:
                 ret = requests.get(url)
             logger.info("Status code is %s, detail is %s.", ret.status_code, ret.json())
             if ret.status_code != 200:
-                raise DmaapClientException('Call dmaap failed. Status code is %s, detail is %s.' % (ret.status_code, ret.json()))
+                raise DmaapClientException(
+                    'Call dmaap failed. Status code is %s, detail is %s.' % (ret.status_code, ret.json()))
             data = ret.json()
             for msg in data:
                 msg = json.loads(msg)
--- catalog/pub/Dmaap_lib/dmaap/identity.py
+++ catalog/pub/Dmaap_lib/dmaap/identity.py
@@ -1,27 +1,30 @@
-# Copyright (c) 2019, CMCC Technologies Co., Ltd.
-# Licensed under the Apache License, Version 2.0 (the "License")
+# Copyright (c) 2019, CMCC Technologies. Co., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# http://www.apache.org/licenses/LICENSE-2.0
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 import json
 import logging
+
 import requests
 
-from ..pub.exceptions import DmaapClientException
+from catalog.pub.Dmaap_lib.pub.exceptions import DmaapClientException
 
 logger = logging.getLogger(__name__)
 
 
 class IdentityClient:
-    def __init__(self, host):
-        self.host = host
+    def __init__(self, base_url):
+        self.base_url = base_url
 
     def create_apikey(self, email, description):
         try:
@@ -31,7 +34,7 @@ class IdentityClient:
                 'description': description
             }
             data = json.JSONEncoder().encode(data)
-            url = "http://%s/apiKeys/create" % (self.host)
+            url = self.base_url + "/apiKeys/create"
             ret = requests.post(url=url, data=data, headers=headers)
             logger.info('create apiKey, response status_code: %s, body: %s', ret.status_code, ret.json())
             if ret.status_code != 200:
@@ -44,3 +47,21 @@ class IdentityClient:
             return resp_data
         except Exception as e:
             raise DmaapClientException('create apikey from dmaap failed: ' + e.message)
+
+    def get_apikey(self, apikey):
+        try:
+            url = self.base_url + "/apiKeys/%s" % apikey
+            ret = requests.get(url)
+            logger.info('get apiKey, response status_code: %s, body: %s', ret.status_code, ret.json())
+            if ret.status_code != 200:
+                raise DmaapClientException(ret.json())
+            ret = ret.json()
+            return ret
+        except Exception as e:
+            raise DmaapClientException('get apikey from dmaap failed: ' + e.message)
+
+    def delete_apikey(self):
+        pass
+
+    def update_apikey(self, apikey, email, description):
+        pass
